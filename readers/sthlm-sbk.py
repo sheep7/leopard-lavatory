347df3400f90a0f78cf74569790f688f4b49fa66
FrontierPsycho <unfortunate42@gmail.com>
2017-09-02 23:23:51 +0200

    Put everything into class, read next page.

--- readers/sthlm-sbk.py
+++ readers/sthlm-sbk.py
@@ -13,81 +13,107 @@ cfg = {'url': 'http://insynsbk.stockholm.se/Byggochplantjansten/Arenden/',
        'user_agent_string': 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6',
        'form_name': 'aspnetForm',
        'address_field_name': 'ctl00$FullContentRegion$ContentRegion$SecondaryContentRegion$SearchPropertyAndCase$SearchProperty$AddressInput',
+       'fastighetsbeteckning_field_name': 'ctl00$FullContentRegion$ContentRegion$SecondaryContentRegion$SearchPropertyAndCase$SearchProperty$PropertyIdInput',
        'search_button_name': 'ctl00$FullContentRegion$ContentRegion$SecondaryContentRegion$SearchPropertyAndCase$SearchButton',
        'search_button_value': 'Sök'}
 
+class SBKReader:
+    def __init__(self, debug=True):
+        self.b = Browser()
+        self.b.addheaders = [('User-agent', cfg['user_agent_string'])]
 
-def fetchResults(address_query_value, cfg):
-    """
-    Fetch the website with the search form, enter the query value
-    and return the resulting html response.
-    mechanize handles all the post/redirect/get [1] confusion nicely
-    for us.
-    [1] https://en.wikipedia.org/wiki/Post/Redirect/Get
-    """
-    b = Browser()
-    b.addheaders = [('User-agent', cfg['user_agent_string'])]
+        logger = logging.getLogger("mechanize")
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+        logger.setLevel(logging.INFO)
+        self.logger = logger
 
-    # avoid 404 roundtrip because of their missing robots.txt
-    b.set_handle_robots(False)
+        # avoid 404 roundtrip because of their missing robots.txt
+        self.b.set_handle_robots(False)
 
-    #########debug##################################################3
-    # Log information about HTTP redirects and Refreshes.
-    b.set_debug_redirects(True)
-    # Print HTTP headers.
-    b.set_debug_http(True)
-    # To make sure you're seeing all debug output:
-    logger = logging.getLogger("mechanize")
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-    logger.setLevel(logging.INFO)
-    #########debug##################################################3
+        if debug:
+            #########debug##################################################3
+            # Log information about HTTP redirects and Refreshes.
+            self.b.set_debug_redirects(True)
+            # Print HTTP headers.
+            self.b.set_debug_http(True)
+            # To make sure you're seeing all debug output:
+            #########debug##################################################3
 
-    b.open(cfg['url'])
-    assert b.viewing_html()
-    logger.info("Got page with title %s" % b.title())
+    def search(self, address_query_value):
+        self.b.open(cfg['url'])
+        assert self.b.viewing_html()
+        self.logger.info("Got page with title %s" % self.b.title())
 
-    # fill form
-    b.select_form(name=cfg['form_name'])
-    b[cfg['address_field_name']] = address_query_value 
+        # fill form
+        self.b.select_form(name=cfg['form_name'])
+        self.b.form.set_all_readonly(False)
 
-    # manual fix: add search button value to post data
-    request = b.form.click()
-    data = request.get_data() + '&' + urllib.urlencode({cfg['search_button_name']: cfg['search_button_value']}) 
-    request.add_data(data)
+        self.b.form[cfg['address_field_name']] = address_query_value
 
-    # send form
-    b.open(request)
+        request = self.b.form.click()
 
-    return b.response().read()
+        data = request.get_data() + '&' + urllib.urlencode({cfg['search_button_name']: cfg['search_button_value']})
+        request.add_data(data)
+
+        # send form
+        self.b.open(request)
+
+        assert self.b.viewing_html()
+        self.logger.info("Got page with title %s" % self.b.title())
+
+        return self.parseResponse(self.b.response().read())
 
 
-def parseResponse(html):
-    """Parse the html for a result table and return
-    the table content in json friendly format"""
+    def nextPage(self):
+        self.b.select_form(name=cfg['form_name'])
+        self.b.form.set_all_readonly(False)
 
-    s = BeautifulSoup(html, 'lxml')
+        self.b.form.new_control("hidden", "__EVENTTARGET", { "value": "ctl00$FullContentRegion$ContentRegion$SecondaryContentRegion$CaseList$CaseGrid", "id": "__EVENTTARGET" })
+        self.b.form.new_control("hidden", "__EVENTARGUMENT", { "value": "Page$Next", "id": "__EVENTARGUMENT" })
 
-    all_rows = s.find_all('tr')
+        request = self.b.form.click()
+        self.b.open(request)
 
-    cases = {}
-    for row in all_rows:
-        cells = row.find_all('td')
-        # use only rows with 5 columns and a specific class to distinguish them from other table elements
-        if len(cells) == 5 and cells[0].get('class') == ['DataGridItemCell']:
-            case_id = cells[0].find_all('a')[0].string
-            case = {}
-            case['fastighet']   = cells[1].string.strip()
-            case['type']        = cells[2].string.strip() 
-            case['description'] = cells[3].string.strip() 
-            case['date']        = cells[4].string.strip() 
-            cases[case_id] = case
+        assert self.b.viewing_html()
+        self.logger.info("Got page with title %s" % self.b.title())
 
-    return cases
+        return self.parseResponse(self.b.response().read())
+
+
+    def parseResponse(self, responseHTML):
+        """Parse the html for a result table and return
+        the table content in json friendly format"""
+
+        s = BeautifulSoup(responseHTML, 'lxml')
+
+        # TODO more specific criteria?
+        all_rows = s.find_all('tr')
+
+        cases = {}
+        for row in all_rows:
+            cells = row.find_all('td')
+            # use only rows with 5 columns and a specific class to distinguish them from other table elements
+            if len(cells) == 5 and cells[0].get('class') == ['DataGridItemCell']:
+                case_id = cells[0].find_all('a')[0].string
+                print "Found case with ID: %s" % case_id
+                case = {}
+                case['fastighet']   = cells[1].string.strip()
+                case['type']        = cells[2].string.strip()
+                case['description'] = cells[3].string.strip()
+                case['date']        = cells[4].string.strip()
+                cases[case_id] = case
+
+        return cases
 
 
 def main():
-    html = fetchResults('Brunnsgatan 1',cfg)
-    cases = parseResponse(html)
+    reader = SBKReader()
+    cases = reader.search("Brunnsgatan 1")
+
+    print "Found %s results:" % len(cases)
+    print json.dumps(cases, indent=2)
+
+    cases = reader.nextPage()
 
     print "Found %s results:" % len(cases)
     print json.dumps(cases, indent=2)
