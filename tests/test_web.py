295e83c597ab44832c6fd186c4bf8cefe307bd90
FrontierPsycho <nonsenseisay@protonmail.com>
2019-02-04 00:03:24 +0100

    Added tests for the delete endpoint.

--- tests/test_web.py
+++ tests/test_web.py
@@ -37,3 +37,22 @@ def test_index_post_invalid_input(client):
     ), follow_redirects=True)
     assert b'fel' in rv.data
     assert b'Aktivera' not in rv.data
+
+
+def test_delete_get_with_token(client):
+    token = 'sometoken'
+    rv = client.get('/delete', query_string=dict(t=token))
+    assert b'<label for="token">' not in rv.data
+    token_value = 'value="%s"' % token
+    assert bytearray(token_value, 'utf-8') in rv.data
+
+
+def test_delete_get_without_token(client):
+    token = 'sometoken'
+    rv = client.get('/delete')
+    assert b'<label for="token">' in rv.data
+    token_value = 'value="%s"' % token
+    assert bytearray(token_value, 'utf-8') not in rv.data
+
+
+# TODO how to test with existing data, ie deleting an account?
